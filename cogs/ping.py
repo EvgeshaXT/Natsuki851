f45e13c034fa4358cbe53817fc9b0a0c88d61737
EvgeshaXT <evgeshaxt@mail.ru>
2025-09-06 19:08:46 +0300

    v0.0.2: Обновлённые cogs/ping.py и README.md Дата и время: Пт 06.09.2025 19:09:09 +03:00
    - Добавлены комментарии в cogs/ping.py
    - Обновлён README.md. Были исправлены отпечатки, а также был добавлен раздел про автозапуск

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,10 +1,13 @@
 from disnake.ext import commands
 
 class PingCog(commands.Cog):
+    """Данный модуль создан исключительно для проверки работоспособности бота"""
     def __init__(self, bot):
         self.bot = bot
 
+    # Используется простой декоратор слушателя событий
     @commands.Cog.listener()
+    # Событие при получении сообщения
     async def on_message(self, message):
         if message.content == f"<@{self.bot.user.id}>":
             await message.reply("Я тут.")
